df2b735da60de96be3e69c1ec2874a95b8fd69db
Quinn Dirks <qdirks@gmail.com>
2023-08-04 03:04:37 -0500

    append script inside iframe to avoid DOM nodes flashing in inspector tools

--- client.js
+++ client.js
@@ -2,24 +2,32 @@ const query = QUERY;
 const src = SIGNAL_SRC;
 let version = window.location.search.slice(1).split("&").find(str=>str.startsWith(query));
 if (version) version = version.split("=")[1];
-setInterval(()=>{
-    const script = document.createElement('script');
-    script.src = src;
-    if (window.location.search.startsWith("?")) {
-        if (!version) window.location = window.location.protocol + '//' + window.location.pathname + window.location.search + "&" + query + "=0";
-    } else window.location = window.location.protocol + '//' + window.location.pathname + "?" + query + "=0";
-    document.children[0].append(script);
-    script.onload = ()=>{
-        if (Number(version) === window[query]) return;
-        if (window.location.search) {
-            const search = window.location.search.slice(1).split("&").reduce((pv, cv)=>{
-                if (cv.startsWith(query)) return pv;
-                pv += cv;
-                return pv;
-            }, '');
-            window.location = window.location.protocol + '//' + window.location.pathname + "?" + query + "=" + window[query] + (search ? "&" + search : '');
+const iframe = document.createElement('iframe');
+iframe.addEventListener('load', ()=>{
+    const win = iframe.contentWindow;
+    setInterval(()=>{
+        const script = win.document.createElement('script');
+        script.src = src;
+        if (window.location.search.startsWith("?")) {
+            if (!version) {
+                window.location = window.location.protocol + '//' + window.location.pathname + window.location.search + "&" + query + "=0";
+            }
+        } else window.location = window.location.protocol + '//' + window.location.pathname + "?" + query + "=0";
+        win.document.body.append(script);
+        script.onload = ()=>{
+            if (Number(version) === win[query]) return;
+            if (window.location.search) {
+                const search = window.location.search.slice(1).split("&").reduce((pv, cv)=>{
+                    if (cv.startsWith(query)) return pv;
+                    pv += cv;
+                    return pv;
+                }, '');
+                window.location = window.location.protocol + '//' + window.location.pathname + "?" + query + "=" + win[query] + (search ? "&" + search : '');
+            }
+            else window.location = window.location.protocol + '//' + window.location.pathname + "?" + query + "=" + win[query];
         }
-        else window.location = window.location.protocol + '//' + window.location.pathname + "?" + query + "=" + window[query];
-    }
-    script.remove();
-}, 1000);
+        script.remove();
+    }, 200);
+});
+iframe.style.display = 'none';
+document.body.append(iframe);
