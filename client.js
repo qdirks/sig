e8df1545866e12d12646d41aeeefa93f6e4822e0
Quinn Dirks <qdirks@gmail.com>
2023-11-18 21:19:39 -0600

    Scope the variables to a block to avoid naming collisions.

--- client.js
+++ client.js
@@ -1,33 +1,36 @@
-const query = QUERY;
-const src = SIGNAL_SRC;
-let version = window.location.search.slice(1).split("&").find(str=>str.startsWith(query));
-if (version) version = version.split("=")[1];
-const iframe = document.createElement('iframe');
-iframe.addEventListener('load', ()=>{
-    const win = iframe.contentWindow;
-    setInterval(()=>{
-        const script = win.document.createElement('script');
-        script.src = src;
-        if (window.location.search.startsWith("?")) {
-            if (!version) {
-                window.location = window.location.protocol + '//' + window.location.pathname + window.location.search + "&" + query + "=0";
+{
+    const query = QUERY;
+    const src = SIGNAL_SRC;
+    let version = window.location.search.slice(1).split("&").find(str=>str.startsWith(query));
+    if (version) version = version.split("=")[1];
+    const iframe = document.createElement('iframe');
+    iframe.addEventListener('load', ()=>{
+        const win = iframe.contentWindow;
+        setInterval(()=>{
+            const script = win.document.createElement('script');
+            script.src = src;
+            if (window.location.search.startsWith("?")) {
+                if (!version) {
+                    window.location = window.location.protocol + '//' + window.location.pathname + window.location.search + "&" + query + "=0";
+                }
+            } else window.location = window.location.protocol + '//' + window.location.pathname + "?" + query + "=0";
+            win.document.body.append(script);
+            script.onload = ()=>{
+                script.onload = undefined;
+                if (Number(version) === win[query]) return;
+                if (window.location.search) {
+                    const search = window.location.search.slice(1).split("&").reduce((pv, cv)=>{
+                        if (cv.startsWith(query)) return pv;
+                        pv += cv;
+                        return pv;
+                    }, '');
+                    window.location = window.location.protocol + '//' + window.location.pathname + "?" + query + "=" + win[query] + (search ? "&" + search : '');
+                }
+                else window.location = window.location.protocol + '//' + window.location.pathname + "?" + query + "=" + win[query];
             }
-        } else window.location = window.location.protocol + '//' + window.location.pathname + "?" + query + "=0";
-        win.document.body.append(script);
-        script.onload = ()=>{
-            if (Number(version) === win[query]) return;
-            if (window.location.search) {
-                const search = window.location.search.slice(1).split("&").reduce((pv, cv)=>{
-                    if (cv.startsWith(query)) return pv;
-                    pv += cv;
-                    return pv;
-                }, '');
-                window.location = window.location.protocol + '//' + window.location.pathname + "?" + query + "=" + win[query] + (search ? "&" + search : '');
-            }
-            else window.location = window.location.protocol + '//' + window.location.pathname + "?" + query + "=" + win[query];
-        }
-        script.remove();
-    }, 200);
-});
-iframe.style.display = 'none';
-document.body.append(iframe);
+            script.remove();
+        }, 1000);
+    });
+    iframe.style.display = 'none';
+    document.body.append(iframe);
+}
